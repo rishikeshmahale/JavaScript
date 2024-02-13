2f97f2e56b3cc001687e61adf1c1636e4bcc7ff9
Rishikesh Mahale <rishikeshmahale16@gmail.com>
2024-02-13 17:46:29 +0530

    new chapters

--- Hitesh Chaudhary/03_basics/04_iife.js
+++ Hitesh Chaudhary/03_basics/04_iife.js
@@ -1,6 +1,35 @@
 
 // Immediately invoked function expression (IIFE)
 
-(function chai() {
-    console.log("DB CONNECTED");
-}())
+
+// When we dont want pollution from global scope we create our seperate scope and when we want the fn to immediately invoke.
+
+
+// Many a times the variables declared in the global scope causes problems and to eliminate that pollution we use iife
+
+
+// () the first parenthesis is where we write the function's defination.
+// () the 2nd parenthesis is when we execute the function.
+
+// named function
+(function One() {
+    console.log("IIFE 1");
+})(); 
+// The semicolor is necessary since the iife doesnt know when to stop executing context so in such situations we use ; to end the execution.
+
+
+(() => {
+    console.log("IIFE 2");
+})();
+
+
+(function myName(name) {
+    console.log(`welcome, ${name}`)
+})('chiekoo');
+
+((name) => {
+    console.log(`welcome, ${name}`)
+})('harry');
+
+
+
