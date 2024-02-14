1524361a7fba8f33787122502ab7ea9e9b6980e7
Rishikesh Mahale <rishikeshmahale16@gmail.com>
2024-02-14 22:59:42 +0530

    new chapters

---
+++ Hitesh Chaudhary/04_control_flow/switch.js
@@ -0,0 +1,37 @@
+console.log("Switches in JS");
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// key => it is the value which i have to check;
+// default => it runs only when none of the cases match.
+// break => if its not mentioned below each case it executes all the condition except default.
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    
+    default:
+        console.log("Default case match");
+        break;
+    
+    
+}
+
