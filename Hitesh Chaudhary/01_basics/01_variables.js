f250af1bf3a668c4dff1100f9b26cb03dd5a4c07
Rishikesh Mahale <rishikeshmahale16@gmail.com>
2024-02-18 11:40:32 +0530

    new chapters

---
+++ Hitesh Chaudhary/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+
+const accountId = 144593;
+
+let email = "xyz@hotmail.com";
+
+
+
+
---
+++ Hitesh Chaudhary/01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all code as newer version of js
+console.log("data types in JavaScript");
+
+// string
+let string = "name";
+
+// number
+let age = 20;
+
+// boolean
+let boolean = "false";
+
+// object
+let obj = {
+    name: "Vegeta",
+    isHuman: true
+};
+
+// array
+const arr = [1, true, "Goku"];
+
+// null => standalone value
+const state = null;
+
+// undefined
+let home;
+
+// symbol => unique
+console.log(typeof arr);
+console.log(typeof obj);
+console.log(typeof state);
+console.log(typeof boolean);
+console.log(typeof home);
+
+
